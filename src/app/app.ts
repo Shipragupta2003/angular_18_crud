89e0960d5673fcadc991f3a81fbd8468e17bf070
Shipra Gupta <shipraagupta1@gmail.com>
2026-07-01 14:36:02 +0530

    crud operations for employees

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,88 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { EmployeeModel } from './models/Employee';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular_18_crud');
+  employeeForm: FormGroup = new FormGroup({});
+  employeeObj: EmployeeModel = new EmployeeModel();
+  employeeList: EmployeeModel[] = [];
+  isEditMode: boolean = false;
+
+  constructor(){
+    this.createForm();
+    const oldData = localStorage.getItem("EmpData");
+    if(oldData!= null){
+      const parseData = JSON.parse(oldData);
+      this.employeeList = parseData;
+    }
+  }
+
+  createForm(){
+    this.employeeForm = new FormGroup({
+      empId: new FormControl(this.employeeObj.empId),
+      name: new FormControl(this.employeeObj.name),
+      city: new FormControl(this.employeeObj.city),
+      address: new FormControl(this.employeeObj.address),
+      contactNo: new FormControl(this.employeeObj.contactNo),
+      emailId: new FormControl(this.employeeObj.emailId),
+      state: new FormControl(this.employeeObj.state),
+      pinCode: new FormControl(this.employeeObj.pinCode)
+    })
+  }
+
+  reset(){
+    this.employeeObj = new EmployeeModel();
+    this.createForm()
+  }
+
+  onSave(){
+    this.isEditMode = false;
+    const oldData = localStorage.getItem("EmpData");
+    if(oldData!= null){
+      const parseData= JSON.parse(oldData);
+      this.employeeForm.controls['empId'].setValue(parseData.length +1);
+      this.employeeList.unshift(this.employeeForm.value);
+
+    }else{
+      this.employeeList.unshift(this.employeeForm.value);
+    }
+    localStorage.setItem("EmpData", JSON.stringify(this.employeeList));
+    this.reset();
+  }
+
+  onEdit(item : EmployeeModel){
+    this.isEditMode = true;
+    this.employeeObj = item;
+    this.createForm();
+
+  }
+
+  onUpdate(){
+   this.isEditMode = false;
+    const record= this.employeeList.find(m=>m.empId== this.employeeForm.controls['empId'].value)
+    if (record!= undefined){
+      record.address= this.employeeForm.controls['address'].value;
+      record.name= this.employeeForm.controls['name'].value;
+      record.contactNo= this.employeeForm.controls['contactNo'].value;
+      record.emailId= this.employeeForm.controls['emailId'].value;
+
+    }
+    localStorage.setItem("EmpData", JSON.stringify(this.employeeList));
+    this.reset();
+  }
+
+  onDelete(id: number){
+    const isDelete= confirm("Are you sure want to Delete?");
+    if(isDelete){
+      const index = this.employeeList.findIndex(m => m.empId == id);
+      this.employeeList.splice(index,1)
+    }
+  }
 }
